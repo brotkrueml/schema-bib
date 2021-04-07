275925d7516fb97e337a312dc2af18a3d685d943
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2021-04-07 17:32:30 +0200

    [TASK] Add header comment

--- Configuration/TxSchema/TypeModels.php
+++ Configuration/TxSchema/TypeModels.php
@@ -1,4 +1,12 @@
 <?php
+
+/*
+ * This file is part of the "schema_bib" extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
 return [
     \Brotkrueml\SchemaBib\Model\Type\Atlas::class,
     \Brotkrueml\SchemaBib\Model\Type\Audiobook::class,
